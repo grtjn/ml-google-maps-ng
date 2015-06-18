e655502610f3ee2614833a67f79c42c62011e88b
Geert Josten <geert.josten@marklogic.com>
2015-06-18 15:15:57 +0200

    Added object2array filter

--- src/ml-google-maps-ng/ml-google-maps.js
+++ src/ml-google-maps-ng/ml-google-maps.js
@@ -1,6 +1,16 @@
 (function() {
   'use strict';
 
-  angular.module('ml.google-maps', ['ui.map']);
+  angular.module('ml.google-maps', ['ui.map'])
+  .filter('object2Array', function() {
+    return function(input) {
+      var out = [];
+      for (var name in input) {
+        input[name].__key = name;
+        out.push(input[name]);
+      }
+      return out;
+    };
+  });
 
 }());
